bc387eff0a57046c61f529a77104fd87aed8f2f4
Sazxt <karuma1363@gmail.com>
2020-07-22 18:30:19 +0700

    ok

---
+++ dipw/out.py
@@ -0,0 +1,12 @@
+# Source Generated with disassembly
+# Timestamp in code: 1595416682 (2020-07-22 18:18:02)
+# File : dp.py
+def main():
+	password = "Call me a Python virtual machine! I can interpret Python bytecodes!!!"
+	if raw_input("password: ") == password:
+		print "hitcon{Now you can compile and run Python bytecode in your brain!}"
+	else:
+		print "Wrong password... Please try again. Do not brute force. =) "
+
+if __name__ == "__main__":
+	main()
